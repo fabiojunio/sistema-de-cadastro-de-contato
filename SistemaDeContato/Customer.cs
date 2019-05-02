4696a39eeb3eecf48eb0db3ef4bd4fd7a7f016d3
Fabio Junio <fabiojunio.louv@gmail.com>
2019-05-02 17:37:01 -0300

    Primeiro Commit

---
+++ SistemaDeContato/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaDeContato
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(10)]
+        public string Name { get; set; }
+    }
+}
---
+++ SistemaDeContato/Pages/Create.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace SistemaDeContato.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        private ILogger<CreateModel> Log;
+
+        public CreateModel(AppDbContext db, ILogger<CreateModel> log)
+        {
+            _db = db;
+            Log = log;
+        }
+
+        [TempData]
+        public string Message { get; set; }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid) { return Page(); }
+
+            _db.Customers.Add(Customer);
+            await _db.SaveChangesAsync(); 
+            var msg = $"Contato {Customer.Name} adicionado com sucesso.";
+            Message = msg;
+            Log.LogCritical(msg);
+            return RedirectToPage("/Index");
+        }
+    }
+}
